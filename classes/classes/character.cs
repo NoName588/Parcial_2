2be6fa50f2830c5eeea365306eb04ea6ea407ef2
jamayav <juan.amayav@upb.edu.co>
2023-04-01 10:48:13 -0500

    code.3

--- classes/classes/character.cs
+++ classes/classes/character.cs
@@ -6,82 +6,23 @@ using System.Threading.Tasks;
 
 namespace classes
 {
-    internal class character
+    internal class character : ArmorandWeapon
     {
-        public string name { get; private set; }
-        public int Hp { get; private set; }
-        public Arma ArmaEquipada { get; private set; }
-        public Armadura ArmaduraEquipada { get; private set; }
-        public int AtkBase { get; private set; }
-        public int DefBase { get; private set; }
-        public TipoPersonaje Clase { get; private set; }
-
-        public Personaje(string nombre, int hp, int atkBase, int defBase, TipoPersonaje clase)
+        string name = "pepe";
+        int life = 15;
+        int atk = 5;
+        int def = 2;
+        public character(string name, int life, int atk, int def,
+                     int def_armor, int atk_arma, int dur_armor,
+                     int dur_arma, bool equiparmor, bool equipweapon)
+                     : base(def_armor, atk_arma, dur_armor, dur_arma, equiparmor, equipweapon)
         {
-            if (hp < 1) throw new ArgumentOutOfRangeException("Hp no puede ser negativo o cero.");
-            Nombre = nombre;
-            Hp = hp;
-            AtkBase = atkBase;
-            DefBase = defBase;
-            Clase = clase;
+            this.name = name;
+            this.life = life;
+            this.atk = atk;
+            this.def = def;
         }
-
-        public bool EquiparArma(Arma arma)
-        {
-            if (arma == null || arma.Durabilidad < 1 || (arma.Clase != Clase && arma.Clase != TipoPersonaje.Any)) return false;
-            ArmaEquipada?.Desequipar();
-            arma.Equipar(this);
-            ArmaEquipada = arma;
-            return true;
-        }
-
-        public bool EquiparArmadura(Armadura armadura)
-        {
-            if (armadura == null || armadura.Durabilidad < 1 || (armadura.Clase != Clase && armadura.Clase != TipoPersonaje.Any)) return false;
-            ArmaduraEquipada?.Desequipar();
-            armadura.Equipar(this);
-            ArmaduraEquipada = armadura;
-            return true;
-        }
-
-        public void RecibirAtaque(Personaje atacante)
-        {
-            int danio;
-            if (ArmaduraEquipada != null)
-            {
-                danio = (int)Math.Floor((double)atacante.AtkBase / 2);
-                danio = ArmaduraEquipada.ReducirDurabilidad(danio);
-                if (danio >= Hp)
-                {
-                    Hp = 0;
-                }
-                else
-                {
-                    Hp -= danio;
-                }
-            }
-            else
-            {
-                danio = atacante.AtkBase;
-                if (danio >= Hp)
-                {
-                    Hp = 0;
-                }
-                else
-                {
-                    Hp -= danio;
-                }
-            }
-        }
-    }
-
-    // Enum TipoPersonaje
-    public enum TipoPersonaje
-    {
-        Human,
-        Beast,
-        Hybrid,
-        Any
-    }
+    } 
+       
 }
-}
+
