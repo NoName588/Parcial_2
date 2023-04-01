40e12ba5f01a3afad7ef30e3f7662be78ace7a08
jamayav <juan.amayav@upb.edu.co>
2023-04-01 09:56:08 -0500

    creation of classes and trials

---
+++ classes/trials/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace trials
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
